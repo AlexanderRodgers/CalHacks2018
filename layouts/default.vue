02be30c12083fcc863620fc821449009c13d8050
Alex Rodgers <alexedrodgers@gmail.com>
2018-11-03 01:30:00 -0700

    started project files

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-toolbar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-toolbar-side-icon @click="drawer = !drawer" />
+      <v-toolbar-title v-text="title"/>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'CancerFinder'
+      }
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center>
+      <v-card>
+        <v-card-title class="headline">
+          Concerned about your skin? You should be.
+        </v-card-title>
+        <v-card-text>
+          We developed a machine learning algorithm to analyze and detect possibly cancerous
+          skin samples using pictures that you securely upload.
+        </v-card-text>
+        <v-card-actions>
+          <v-btn>Find out</v-btn>
+        </v-card-actions>
+      </v-card>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
